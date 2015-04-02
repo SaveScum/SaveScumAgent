3d6d4562651af6bcf27528a1ea9b31a113711631
Steve <SaveScum@users.noreply.github.com>
2015-04-02 02:48:20 -0700

    Creating data interface
    Need to remove dependency on Archiver.ArchiveFormats.

---
+++ SaveScumAgent.Data/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveScumAgent.Data.Models
+{
+    public class Game
+    {
+        public virtual GameSettings Settings { get; set; }
+    }
+}
---
+++ SaveScumAgent.Data/Models/GameSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SaveScumAgent.Archiver.Formats;
+
+namespace SaveScumAgent.Data.Models
+{
+    public class GameSettings
+    {
+        public ArchiveFormats Format { get; set; }
+
+        public virtual Game Game { get; set; }
+    }
+}
