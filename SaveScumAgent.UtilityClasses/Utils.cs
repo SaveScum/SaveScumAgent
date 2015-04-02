d2062bc2ab210ae17c2c2a205aef4cf831fbc852
Steve <SaveScum@users.noreply.github.com>
2015-04-02 00:24:44 -0700

    Added FormatWith extension method
    From James Newton-King

--- SaveScumAgent.UtilityClasses/Utils.cs
+++ SaveScumAgent.UtilityClasses/Utils.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web.UI;
 
 namespace SaveScumAgent.UtilityClasses
 {
@@ -34,17 +37,77 @@ namespace SaveScumAgent.UtilityClasses
         }
 
         /// <summary>
-        /// Gets an attribute on an enum field value
+        ///     Gets an attribute on an enum field value
         /// </summary>
         /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
         /// <param name="enumVal">The enum value</param>
         /// <returns>The attribute of type T that exists on the enum value</returns>
-        public static T GetAttributeOfType<T>(this Enum enumVal) where T : System.Attribute
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T : Attribute
         {
             var type = enumVal.GetType();
             var memInfo = type.GetMember(enumVal.ToString());
-            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
-            return (attributes.Length > 0) ? (T)attributes[0] : null;
+            var attributes = memInfo[0].GetCustomAttributes(typeof (T), false);
+            return (attributes.Length > 0) ? (T) attributes[0] : null;
+        }
+
+        /// <summary>
+        ///     Courtesy of James Newton-King
+        ///     http://james.newtonking.com/archive/2008/03/29/formatwith-2-0-string-formatting-with-named-variables
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string format, object source)
+
+        {
+            return FormatWith(format, null, source);
+        }
+
+        /// <summary>
+        ///     Courtesy of James Newton-King
+        ///     http://james.newtonking.com/archive/2008/03/29/formatwith-2-0-string-formatting-with-named-variables
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="provider"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string format, IFormatProvider provider, object source)
+
+        {
+            if (format == null)
+
+                throw new ArgumentNullException("format");
+
+
+            var r = new Regex(@"(?<start>\{)+(?<property>[\w\.\[\]]+)(?<format>:[^}]+)?(?<end>\})+",
+                RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+
+            var values = new List<object>();
+
+            var rewrittenFormat = r.Replace(format, delegate(Match m)
+
+            {
+                var startGroup = m.Groups["start"];
+
+                var propertyGroup = m.Groups["property"];
+
+                var formatGroup = m.Groups["format"];
+
+                var endGroup = m.Groups["end"];
+
+
+                values.Add((propertyGroup.Value == "0")
+                    ? source
+                    : DataBinder.Eval(source, propertyGroup.Value));
+
+
+                return new string('{', startGroup.Captures.Count) + (values.Count - 1) + formatGroup.Value
+                       + new string('}', endGroup.Captures.Count);
+            });
+
+
+            return string.Format(provider, rewrittenFormat, values.ToArray());
         }
     }
 }
