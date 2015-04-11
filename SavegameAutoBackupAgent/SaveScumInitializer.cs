5a66f5233fc5412901adabefd0ecb830eb03f7f5
Steve <SaveScum@users.noreply.github.com>
2015-04-11 03:37:00 -0700

    Code cleanup

--- SavegameAutoBackupAgent/SaveScumInitializer.cs
+++ SavegameAutoBackupAgent/SaveScumInitializer.cs
@@ -1,35 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.SqlServer;
-using System.Diagnostics;
-using System.Environment;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using SaveScumAgent.Properties;
 using SaveScumAgent.UtilityClasses;
 
 namespace SaveScumAgent
 {
-    class SaveScumInitializer
+    internal class SaveScumInitializer
     {
         public static void SetupDataDirectory()
         {
             EnsureDataDirectoryExists();
             CreateAppConfig();
-
         }
 
         private static void EnsureDataDirectoryExists()
         {
-            var dataDir = Properties.Settings.Default.SaveScumAppDataDirectory.FormatWith(SpecialFolderHelper.PathsDictionary);
+            var dataDir = Settings.Default.SaveScumAppDataDirectory.FormatWith(SpecialFolderHelper.PathsDictionary);
             if (!Directory.Exists(dataDir))
                 Directory.CreateDirectory(dataDir);
         }
 
         private static void CreateAppConfig()
         {
-            
         }
     }
 }
